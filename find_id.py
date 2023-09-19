12c04a3913fc3ac6012ceaa17fe705b5f4ec82ef
marius.sutkus <marius.sutkus@ext.fmc-ag.com>
2023-09-19 17:30:32 +0300

    -added new functions to serach for pattern in funstions

--- find_id.py
+++ find_id.py
@@ -4,59 +4,94 @@ from argparse import ArgumentParser
 from pathlib import Path 
 
 
-class TestFind:
-	start_nr: str = "4AP"
-	file_ext: str = "*.py"
-	start_id: str = 'Polarion ID'
+def id_number(row: str):
+    if (index:= row.find("4AP2-")) !=-1:
+        return row[index:].split(' ')[0]
+    else:
+        return None
+
+def find_new_pattern(text_dict, keys):
+    rows_with_patterns: list = []
+    new_patterns: list = []
+    for key in keys:
+ 
+        for i in range(key):
+            key -=1
+            if text_dict[key].find('if __name__ =="__main__":') !=-1:
+                break
+            if text_dict[key].find('def ') !=-1:
+                rows_with_patterns.append(text_dict[key])
+                break
+    for pattern in rows_with_patterns:
+        if (index:= pattern.find("def ")) !=-1:
+            new_patterns.append('.' + pattern[index+4:].split('(')[0])
+    return new_patterns
 
 
-	def __init__(self):
-		self.source_path: Path = Path("C:/Users/marius.sutkus.QDTEAM/Documents/training/code/test2") #args.DIR
-		self.pattern: str = 'hydraulics_connetor.reconnect_shunts('  #args.PATTERN
-		self.id_list: list = []
+def find_function_name(path, find_pattern):
+    key_list = []
+    count = 0
+    text_dict: dict = {}
+    read_text = Path(path).read_text(encoding='utf-8')
+    for row in read_text.split('\n'):
+        text_dict[count]=row
+        count+=1
+    for key, value in text_dict.items():
+        if value.find(find_pattern) != -1:
+          key_list.append(key)
+    return find_new_pattern(text_dict=text_dict, keys=key_list)
 
 
+def find_tests(source_path: Path = None, find_pattern: str = None) -> list:
+    id_list: list = []
+    path_list: list = []
+    for file_name in source_path.rglob("*.py"):
+        test_id: str = None
+        read_text: str = Path(file_name).read_text(encoding='utf-8')
 
-	def id_number(self, row):
-		index = row.find(self.start_nr)
-		return row[index:index+10]
-
-
-	def find_files(self):
-		return self.source_path.rglob(self.file_ext)
-
-	def find_tests(self):
-
-		list_of_files = self.find_files()
-		# print(sorted(list_of_files))
-		for file_name in list_of_files:
-			read_text = Path(file_name).read_text()
-		
-			for row in read_text:
-				print(row)
-				if row.find(self.pattern) != -1:
-					for row2 in read_text:
-						if row2.find(self.start_id) != -1:
-							self.id_list.append(self.id_number(row=row2))
-		print(self.id_list)
-		
-		# return set(id_list)
-	
+        for row in read_text.split('\n'):
+            if row.find("Polarion ID") != -1:
+                test_id = id_number(row=row)
+            if row.find(find_pattern) != -1:
+                if test_id:
+                    id_list.append(test_id)
+                else:
+                    path_list.append(file_name)
+    
+    return id_list, path_list
 
 if __name__ =="__main__":
 
-	# parser: ArgumentParser = ArgumentParser(description="Search for test case ID number, which contains the pattern.")
-	# parser.add_argument("PATTERN", help="Pattern to search for", type=str)
-	# parser.add_argument("DIR", help="Directory to search in", type=Path)
-	# args = parser.parse_args()
-	find_tc = TestFind()
-	find_tc.find_tests()
+    # parser: ArgumentParser = ArgumentParser(description="Search for test case ID number, which contains the pattern.")
+    # parser.add_argument("PATTERN", help="Pattern to search for", type=str)
+    # parser.add_argument("DIR", help="Directory to search in", type=Path)
+    # args = parser.parse_args()
 
-	# path = Path("C:/Users/marius.sutkus.QDTEAM/Documents/training/code/TestAutomation")
-	# list_of_files = path.rglob('*.py')
-	# for file in list_of_files:
-	# 	print(file)
-	# print(list)
+    # file_path = Path('C:/Users/marius.sutkus.QDTEAM/Documents/training/code/TestAutomation/procedures/program/cleaning.py')
+    source_path = Path("C:/Users/marius.sutkus.QDTEAM/Documents/training/code/TestAutomation")
+    # pattern = 'hydraulics_connector.reconnect_shunts('
+    # pattern = 'cleaning.start_cleaning('
+    pattern = 'reconnect_shunts('
 
-	# with open('search_results.txt', 'w') as results:
-	# 	results.write(str(find_tests(source_path=args.DIR, find_string=args.PATTERN)))
+    # source_path = Path("C:/Users/marius.sutkus.QDTEAM/Documents/training/code/test2")
+    # pattern = 'file_name).read_text('
+    test_id: list = []
+    id_list, path_list = find_tests(source_path=source_path, find_pattern=pattern)
+    print(path_list)
+    # test_id.extend([id for id in set(id_list)])
+    test_id.extend(id_list)
+    patterns: list = []
+    for path in path_list:
+        patterns.extend(find_function_name(path=path, find_pattern=pattern))
+    print(patterns)
+    new_patterns: list = []
+    for pattern in patterns:
+        id_list, path_list = find_tests(source_path=source_path, find_pattern=pattern)
+        test_id.append(id_list)
+        print(path_list)
+        new_patterns.extend(path_list)
+
+    
+    # print(test_id)
+    # print(len(test_id))
+    # print(new_patterns)
