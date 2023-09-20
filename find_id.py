d390376f07fc402499dd3a8f2172e1639257f4f1
marius.sutkus <marius.sutkus@ext.fmc-ag.com>
2023-09-20 18:00:55 +0300

    -fixed issues related to search for pattern in functions

--- find_id.py
+++ find_id.py
@@ -1,97 +1,225 @@
 import os
 from argparse import ArgumentParser
 
-from pathlib import Path 
+from pathlib import Path
 
 
-def id_number(row: str):
-    if (index:= row.find("4AP2-")) !=-1:
-        return row[index:].split(' ')[0]
+def id_number(row: str) -> str:
+    index = row.find("4AP2-")
+    if index != -1:
+        return row[index:].split(" ")[0]
     else:
-        return None
+        return ""
 
-def find_new_pattern(text_dict, keys):
+def find_space_count(row: str, pattern: str) -> int:
+    count = None
+    row_split = row.split(' ')
+    for string in row_split:
+        if string.find(pattern) !=-1:
+
+            index = row.find(string)
+            if index != -1:
+                if index > 0:
+                    spaces = row[:index]
+                    if spaces.isspace():
+                        count = len(spaces)
+                else:
+                    count = 0
+    print(row)
+    print(pattern)
+    print(count)
+    return count
+def find_class_method(row: int, text: str)-> str:
+    for i in reversed(range(row)):
+            
+            if text[i].find("class ") != -1:
+                return text[i]
+    return ""
+
+
+def find_new_pattern(read_text, idx_list, pattern, path): # todo manage function in function by counting white spacec before def?
     rows_with_patterns: list = []
     new_patterns: list = []
-    for key in keys:
- 
-        for i in range(key):
-            key -=1
-            if text_dict[key].find('if __name__ =="__main__":') !=-1:
-                break
-            if text_dict[key].find('def ') !=-1:
-                rows_with_patterns.append(text_dict[key])
-                break
+    for idx in idx_list:
+        
+        count_p = find_space_count(row=read_text[idx], pattern=pattern)
+        if not count_p or count_p < 4:
+            continue
+        
+        for i in reversed(range(idx)):
+            
+            if read_text[i].find("def ") != -1:
+                count_d = find_space_count(read_text[i], 'def')
+                if count_p > count_d:
+                    if read_text[i-1].find("@classmethod")==-1:
+                        
+                        rows_with_patterns.append(read_text[i])
+                        break
+                    elif read_text[i-1].find("@classmethod")!=-1:
+                        class_pattern = find_class_method(row=i, text=read_text)
+                        print(path)
+                        print(class_pattern)
+                        print('def')
+                        print(read_text[i])
+                        print(count_d)
+                        # rows_with_patterns.append(class_pattern)
+                        break
+
+                elif count_p <=count_d:
+                    continue
+                
     for pattern in rows_with_patterns:
-        if (index:= pattern.find("def ")) !=-1:
-            new_patterns.append('.' + pattern[index+4:].split('(')[0])
+        index_d = pattern.find("def ")
+        # index_c = pattern.find("class ")
+
+        if index_d != -1:
+            new_patterns.append("." + pattern[index_d + 4 :].split("(")[0]+"(")
+        # if index_c != -1:
+        #     new_patterns.append("." + pattern[index + 4 :].split("(")[0]+"(")
+
     return new_patterns
 
 
 def find_function_name(path, find_pattern):
-    key_list = []
+    idx_list = []
     count = 0
-    text_dict: dict = {}
-    read_text = Path(path).read_text(encoding='utf-8')
-    for row in read_text.split('\n'):
-        text_dict[count]=row
-        count+=1
-    for key, value in text_dict.items():
-        if value.find(find_pattern) != -1:
-          key_list.append(key)
-    return find_new_pattern(text_dict=text_dict, keys=key_list)
+    read_text = Path(path).read_text(encoding="utf-8").split("\n")
+    # for row in read_text.split("\n"):
+    #     text_dict[count] = row
+    #     count += 1
+    for idx, text in enumerate(read_text):
+        if text.find(find_pattern) != -1:
+            idx_list.append(idx)
+    return find_new_pattern(read_text=read_text, idx_list=idx_list, pattern=find_pattern, path=path)
 
 
-def find_tests(source_path: Path = None, find_pattern: str = None) -> list:
+def find_tests(source_path: Path, find_pattern: str) -> tuple[list,list]:
     id_list: list = []
     path_list: list = []
     for file_name in source_path.rglob("*.py"):
-        test_id: str = None
-        read_text: str = Path(file_name).read_text(encoding='utf-8')
+        test_id: str = ""
+        read_text: str = Path(file_name).read_text(encoding="utf-8").split("\n")
 
-        for row in read_text.split('\n'):
-            if row.find("Polarion ID") != -1:
+        for row in read_text:
+            if row.find("Polarion ID") != -1: #and not test_id
                 test_id = id_number(row=row)
-            if row.find(find_pattern) != -1:
-                if test_id:
-                    id_list.append(test_id)
-                else:
-                    path_list.append(file_name)
-    
+                break
+        
+        for row in read_text:
+
+            if row.find(find_pattern) != -1 and test_id:
+                # if test_id:
+                id_list.append(test_id)
+                break
+            if row.find(find_pattern) != -1 and not test_id:
+                print(file_name)
+                path_list.append(file_name)
+                break
+            else:
+                continue
+
     return id_list, path_list
 
-if __name__ =="__main__":
+def start_function(source_path: Path, pattern: str) -> list:
+    pattern_dict = {pattern:0}
+    test_ids: list = []
 
+    for i in range(6):
+        new_paths: list = []
+        new_patterns: list =[]
+        for pat, value in pattern_dict.items():
+            if value == 0:            # 
+                print(pat)
+                id_list, path_list = find_tests(source_path=source_path, find_pattern=pat)
+                test_ids.extend(id_list)
+                for path in path_list:
+                    new_patterns.extend(find_function_name(path=path, find_pattern=pat))
+                pattern_dict[pat]=1
+
+        for pattern in new_patterns:
+                    if pattern not in pattern_dict:
+                        pattern_dict[pattern]=0
+    print(len(test_ids))
+    print(pattern_dict)
+    return test_ids
+
+
+if __name__ == "__main__":
     # parser: ArgumentParser = ArgumentParser(description="Search for test case ID number, which contains the pattern.")
     # parser.add_argument("PATTERN", help="Pattern to search for", type=str)
     # parser.add_argument("DIR", help="Directory to search in", type=Path)
     # args = parser.parse_args()
 
     # file_path = Path('C:/Users/marius.sutkus.QDTEAM/Documents/training/code/TestAutomation/procedures/program/cleaning.py')
-    source_path = Path("C:/Users/marius.sutkus.QDTEAM/Documents/training/code/TestAutomation")
-    # pattern = 'hydraulics_connector.reconnect_shunts('
-    # pattern = 'cleaning.start_cleaning('
+    source_path = Path(
+        "C:/Users/marius.sutkus.QDTEAM/Documents/training/code/TestAutomation"
+    )
     pattern = 'reconnect_shunts('
+    # pattern = 'cleaning.start_cleaning('
+    print (start_function(source_path=source_path, pattern=pattern))
+
+
+
+
+
+    # pattern = "reconnect_shunts("
 
     # source_path = Path("C:/Users/marius.sutkus.QDTEAM/Documents/training/code/test2")
     # pattern = 'file_name).read_text('
-    test_id: list = []
-    id_list, path_list = find_tests(source_path=source_path, find_pattern=pattern)
-    print(path_list)
-    # test_id.extend([id for id in set(id_list)])
-    test_id.extend(id_list)
-    patterns: list = []
-    for path in path_list:
-        patterns.extend(find_function_name(path=path, find_pattern=pattern))
-    print(patterns)
-    new_patterns: list = []
-    for pattern in patterns:
-        id_list, path_list = find_tests(source_path=source_path, find_pattern=pattern)
-        test_id.append(id_list)
-        print(path_list)
-        new_patterns.extend(path_list)
+    # test_ids: list = []
+    # pattern_dict = {pattern:0}
+    # id_list, path_list = find_tests(source_path=source_path, find_pattern=pattern)
+    # test_ids.extend(id_list)
+    # print(test_ids)
+    # print(path_list)
+    # patterns: list = []
+    # for path in path_list:
+    #     patterns.extend(find_function_name(path=path, find_pattern=pattern))
+    
+    # for pattern in patterns:
+    #     if pattern in pattern_dict:
+    #         pattern_dict[pattern]=1
+    #     else:
+    #         pattern_dict[pattern]=0
+
+
+
+    # new_paths: list = []
+    # new_patterns: list =[]
+    # for pat, value in pattern_dict.items():
+    #     if value == 0:
+    #         id_list, path_list = find_tests(source_path=source_path, find_pattern=pat)
+    #     # print(path_list)
+    #         test_ids.extend(id_list)
+    #         for path in path_list:
+    #             new_patterns.extend(find_function_name(path=path, find_pattern=pat))
+    #         pattern_dict[pat]=1
+
+    # for pattern in new_patterns:
+    #     if pattern not in pattern_dict:
+    #         pattern_dict[pattern]=0
 
     
-    # print(test_id)
-    # print(len(test_id))
-    # print(new_patterns)
+    # for i in range(6):
+    #     new_paths: list = []
+    #     new_patterns: list =[]
+    #     for pat, value in pattern_dict.items():
+    #         if value == 0:            # 
+    #             print(pat)
+    #             id_list, path_list = find_tests(source_path=source_path, find_pattern=pat)
+    #             # print(path_list)
+    #             test_ids.extend(id_list)
+    #             for path in path_list:
+    #                 new_patterns.extend(find_function_name(path=path, find_pattern=pat))
+    #             pattern_dict[pat]=1
+    #             # id_list, path_list = [], []
+
+
+    #         else:
+    #             continue
+
+    #     for pattern in new_patterns:
+    #                 if pattern not in pattern_dict:
+    #                     pattern_dict[pattern]=0
+
+
